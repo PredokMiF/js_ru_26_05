e778ea367563fa0b8afba8d5436ce7831ac3dad6
Roman <kaboom.ua@gmail.com>
2016-06-12 15:26:32 -0700

    Update contentToggler.js

--- src/decorators/contentToggler.js
+++ src/decorators/contentToggler.js
@@ -12,6 +12,11 @@ export default (CustomComponent) => class DecoratedByContentToggler extends Comp
     }
 
     componentWillReceiveProps(nextProps) {
+        //это вообще ужасный паттерн, и делать стейт зависимым от пропсов стоит только когда другого выбора совсем нет
+        //иначе вы будете бороться с кучей подобных багов
+        //конкретно в этом случае вы ничего не починили, а проблема в том, что при добавлении комента все дерево перестраивается
+        //И вы так и передаете сверху isOpen={false} хотя в стейте у вас isOpen={true}. Вот и закрываються они придобавлении.
+        //Всегда сохраняйте single source of truth - а у вас вышло 2 источника одних данных
         if (this.state.isOpen != nextProps.isOpend)
             this.setState({isOpen: nextProps.isOpend})
     }
